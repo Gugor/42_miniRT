f119013873a2806d3ddfc70719da111149d8c7de
Marti Marsa Samper <mmarsa-s@car14s4.42barcelona.com>
2025-01-07 17:52:18 +0100

    norm

--- srcs/math-rand-generation.c
+++ srcs/math-rand-generation.c
@@ -16,44 +16,44 @@
 
 float random_float()
 {
-    return (rand() / (RAND_MAX + 1.0));
+	return (rand() / (RAND_MAX + 1.0));
 }
 
 float random_float_range(float min, float max)
 {
-//    float range = (max - min); 
-//    float div = RAND_MAX / range;
-   return (min + (max - min) * random_float());
-    // return min + (max-min)*random_float();
+//	float range = (max - min); 
+//	float div = RAND_MAX / range;
+	return (min + (max - min) * random_float());
+	// return min + (max-min)*random_float();
 }
 
 t_vec3 random_v3()
 {
-    return (vec3(random_float(), random_float(), random_float()));
+	return (vec3(random_float(), random_float(), random_float()));
 }
 
 t_vec3 random_range_v3(float min, float max)
 {
-    return (vec3(random_float_range(min, max), random_float_range(min, max),
-            random_float_range(min, max)));
+	return (vec3(random_float_range(min, max), random_float_range(min, max),
+			random_float_range(min, max)));
 }
 
 t_vec3  random_unit_vector()
 {
-    while (true)
-    {
-        t_vec3 p = random_range_v3(-1,1);
-        float lensq = length_v3(p) * length_v3(p);
-        if (1e-160 < fabs(lensq) && lensq <= 1.0)
-            return div_v3_dbl(p, sqrt(lensq));
-    }
+	while (true)
+	{
+		t_vec3 p = random_range_v3(-1,1);
+		float lensq = length_v3(p) * length_v3(p);
+		if (1e-160 < fabs(lensq) && lensq <= 1.0)
+			return div_v3_dbl(p, sqrt(lensq));
+	}
 }
 
 t_vec3 random_on_hemisphere(const t_vec3 normal)
 {
-    t_vec3 on_unit_sphere = random_unit_vector();
-    if (dot(&on_unit_sphere, &normal) > 0.0) // In the same hemisphere as the normal
-        return (on_unit_sphere);
-    else
-        return (scale_v3(on_unit_sphere, -1));
+	t_vec3 on_unit_sphere = random_unit_vector();
+	if (dot(&on_unit_sphere, &normal) > 0.0) // In the same hemisphere as the normal
+		return (on_unit_sphere);
+	else
+		return (scale_v3(on_unit_sphere, -1));
 }
