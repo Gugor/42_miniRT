23b09a5b40cde3514673b9b528a688b2ef68595e
Marti Marsa Samper <mmarsa-s@car15s1.42barcelona.com>
2025-01-20 15:40:11 +0100

    parsing debugged

--- srcs/parse-errors.c
+++ srcs/parse-errors.c
@@ -18,7 +18,8 @@
 */
 void err_no_rt_extension(void)
 {
-	ft_putstr_fd(2, "Error: this is not a valid file. You need to provide a file with a '.rt' extension\n");	
+	ft_putstr_fd(2, "Error: this is not a valid file. You need to provide a file with a '.rt' extension\n");
+	exit(101);	
 }
 
 /**
@@ -29,6 +30,7 @@ void err_rt_file_not_open(const int err)
 	ft_putstr_fd(2,"Error: ");
 	ft_putstr_fd(2, strerror(err));
 	ft_putstr_fd(2,"\n");
+	exit(101);
 }
 
 void	err_rt_file_format(const char *msg)
@@ -37,7 +39,7 @@ void	err_rt_file_format(const char *msg)
 	ft_putstr_fd(2, (char *)msg);
 	ft_putstr_fd(2,"\n");
 	//clean_scene();
-	exit (101);
+	exit(101);
 }
 
 /**
@@ -45,5 +47,6 @@ void	err_rt_file_format(const char *msg)
 */
 void	err_wrong_rt_file_format(void)
 {
-	ft_putstr_fd(2, "Error: this file has not a valid format. Please provide a valid formatted file.\n");	
+	ft_putstr_fd(2, "Error: this file has not a valid format. Please provide a valid formatted file.\n");
+	exit(101);
 }
